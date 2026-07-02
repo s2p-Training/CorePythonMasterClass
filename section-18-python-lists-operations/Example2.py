3c750879d0d796b55b59e93a00cce7442b3c879d
pranay <pranayramtek613@gmail.com>
2026-07-02 06:57:29 +0530

    Python Master Class

--- section-18-python-lists-operations/Example2.py
+++ section-18-python-lists-operations/Example2.py
@@ -1,1 +1,73 @@
-# Methods On Lists
+# List Slicing
+list6 = ['A','B','C','D','E','F']
+
+sub_list1 = list6[0:1]
+print(sub_list1)
+
+sub_list2 = list6[0:2]
+print(sub_list2)
+
+sub_list3 = list6[0:3]
+print(sub_list3)
+
+sub_list4 = list6[0:4]
+print(sub_list4)
+
+sub_list5 = list6[0:5]
+print(sub_list5)
+
+sub_list6 = list6[0:6]
+print(sub_list6)
+
+# Starting from index 1
+sub_list7 = list6[1:2]
+print(sub_list7)
+
+sub_list8 = list6[1:3]
+print(sub_list8)
+
+sub_list9 = list6[1:4]
+print(sub_list9)
+
+sub_list10 = list6[1:5]
+print(sub_list10)
+
+sub_list11 = list6[1:6]
+print(sub_list11)
+
+# Starting from index 2
+sub_list12 = list6[2:3]
+print(sub_list12)
+
+sub_list13 = list6[2:4]
+print(sub_list13)
+
+sub_list14 = list6[2:5]
+print(sub_list14)
+
+sub_list15 = list6[2:6]
+print(sub_list15)
+
+# Starting from index 3
+sub_list16 = list6[3:4]
+print(sub_list16)
+
+sub_list17 = list6[3:5]
+print(sub_list17)
+
+sub_list18 = list6[3:6]
+print(sub_list18)
+
+# Starting from index 4
+sub_list19 = list6[4:5]
+print(sub_list19)
+
+sub_list20 = list6[4:6]
+print(sub_list20)
+
+# Starting from index 5
+sub_list21 = list6[5:6]
+print(sub_list21)
+
+sub_list22 = list6[6:6]
+print(sub_list22)
